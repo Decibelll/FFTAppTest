25b6af074a5dfd7784e8f22ae68a8784e9827d2a
Sergey Nesteruk <dj_decibell@mail.ru>
2015-12-02 23:42:58 +0200

    Implemented basic fourier transform

--- FFTApp/FFTApp/ViewController.m
+++ FFTApp/FFTApp/ViewController.m
@@ -17,7 +17,28 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
+    
+    fftw_complex *ina, *outa;
+    fftw_plan p;
+    
+    int N = 5;
+    ina = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * N);
+    for( int i = 0; i < N; i++ )
+    {
+        ina[i][0] = 10 * pow( -1, i );
+        ina[i][1] = 0;
+    }
+    outa = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * N);
+    p = fftw_plan_dft_1d(N, ina, outa, FFTW_FORWARD, FFTW_ESTIMATE);
+    
+    fftw_execute(p); /* repeat as needed */
+    
+    for( int i = 0; i < N; i++ )
+    {
+        NSLog( @"Value %i is %f + i* %f", i, outa[i][0], outa[i][1] );
+    }
+    fftw_destroy_plan(p);
+    fftw_free(ina); fftw_free(outa);
     // Do any additional setup after loading the view, typically from a nib.
 }
 
